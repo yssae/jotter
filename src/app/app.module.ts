507c3b50058b4b563f1445d9d00613d9ea6051c1
alyssaelesterio <aklelesterio@gmail.com>
2022-08-21 17:03:01 +0800

    JTR-14 contact-us section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './shared/app-material.module';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { HomeComponent } from './home/home.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './home/navbar/navbar.component';
 import { GetStartedComponent } from './home/get-started/get-started.component';
 import { FeatureComponent } from './home/feature/feature.component';
+import { ContactUsComponent } from './home/contact-us/contact-us.component';
 
 
 @NgModule({
@@ -18,12 +21,14 @@ import { FeatureComponent } from './home/feature/feature.component';
     HomeComponent,
     NavbarComponent,
     GetStartedComponent,
-    FeatureComponent
+    FeatureComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/contact-us/contact-us.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, Validators} from '@angular/forms';
+
+import { Message } from './message';
+
+@Component({
+  selector: 'contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.scss']
+})
+export class ContactUsComponent implements OnInit {
+  contactForm: FormGroup;
+  submitted: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      email: [
+        '',
+        [Validators.required,Validators.email]
+      ],
+      name: [ '', Validators.required],
+      message: ['', Validators.required]
+    })
+  }
+
+  onSubmit(message: Message) {
+    this.submitted = true;
+    if(!message || this.contactForm.invalid) {
+      return;
+    }
+
+    console.log(message);
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+}
