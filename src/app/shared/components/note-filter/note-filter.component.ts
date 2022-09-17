d8541bbe88b7e92dfd5e81d169a079ae8e851408
alyssaelesterio <aklelesterio@gmail.com>
2022-09-17 13:37:48 +0800

    JTR-24 note filter screen added

---
+++ src/app/shared/components/note-filter/note-filter.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+// import { NbToolsComponent } from '../nb-tools/nb-tools.component';
+// import { TextEditorComponent } from '../text-editor/text-editor.component';
+// import { toolType } from 'src/app/_models/toolType';
+
+@Component({
+  selector: 'note-filter',
+  templateUrl: './note-filter.component.html',
+  styleUrls: ['./note-filter.component.scss']
+})
+export class NoteFilterComponent implements OnInit {
+  toolType: any;
+
+  @Input() pageTitle: string;
+  @Input() filterMenu: any;
+
+  constructor(
+    private dialog: MatDialog,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(data => {
+      this.toolType = data;
+    });
+  }
+
+  onOpenTool() {
+    // open Notebook CreateEdit
+    // or open Note > Add Note
+    if(!this.toolType){
+      return;
+    }
+
+    // if(this.toolType.type == 1){
+    //   this.dialog.open(
+    //     NbToolsComponent,
+    //     {
+    //       panelClass:'modal',
+    //       data: {type: 'Create'}
+    //   });
+    // }
+
+    // if(this.toolType.type == 2){
+    //   this.dialog.open(TextEditorComponent,{id:'editor'});
+    // }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { AppMaterialModule } from './app-material.module';
 import { JtrbackbuttonComponent } from './components/jtrbackbutton/jtrbackbutton.component';
+import { NoteFilterComponent } from './components/note-filter/note-filter.component';
 
 @NgModule({
   declarations: [
-    JtrbackbuttonComponent
+    JtrbackbuttonComponent,
+    NoteFilterComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    AppMaterialModule
   ],
-  exports: [JtrbackbuttonComponent]
+  exports: [
+    JtrbackbuttonComponent,
+    NoteFilterComponent
+  ]
 })
 export class SharedModule { }
