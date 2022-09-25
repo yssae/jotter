b3a3bd3d33e8f702ffd5ad880bb80aa573b96639
alyssaelesterio <aklelesterio@gmail.com>
2022-09-25 13:27:25 +0800

    footer icon code clean-up

--- src/app/home/footer/footer.component.ts
+++ src/app/home/footer/footer.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  footerIcons = [
+    { name: 'location', icon: 'location_on', link: 'https://goo.gl/maps/UXK4C4tft4DxwqkC9', description: 'Manila, Philippine' },
+    { name: 'phone', icon: 'call', link: 'tel:+155512345', description: '+1 555 12345' },
+    { name: 'email', icon: 'send', link: 'mailto: support@jotter.com', description: 'support@jotter.com' }
+  ];
 
   constructor() { }
 
