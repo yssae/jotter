07a84c2791512de5d7f569af79270f5076f2c2de
alyssaelesterio <aklelesterio@gmail.com>
2023-04-23 16:01:40 +0800

    JTR-39 notebook clean up

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,26 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AppMaterialModule } from '../shared/app-material.module';
 import { FeatureRoutingModule } from './feature-routing.module';
-import { SharedModule } from '../shared/shared.module';
 import { AccountModule } from '@jtr/feature/account';
-import { NotesModule } from '@jtr/feature/notes';
-import { QuillModule } from 'ngx-quill';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule,
     RouterModule,
-    ReactiveFormsModule,
     FeatureRoutingModule,
-    SharedModule,
     AppMaterialModule,
-    QuillModule,
     AccountModule,
-    NotesModule,
+
   ]
 })
 export class FeatureModule { }
