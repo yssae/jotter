583703c8ec179b417de310e9458ec83faeff289a
alyssaelesterio <aklelesterio@gmail.com>
2023-05-30 16:55:09 +0800

    JTR-45 fetch notes per notebook

---
+++ src/app/feature/services/note.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { JtrDialogService } from '@jtr/shared';
+import { environment } from 'src/environments/environment';
+import { ENDPOINT } from 'src/app/shared/constants/endpoint.const';
+import { Note } from 'src/app/shared/models/note.model';
+import { map, tap, filter, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+
+  constructor(
+    private http: HttpClient,
+    public jtr: JtrDialogService
+  ) { }
+
+  createNote(note: Note) {
+    const url = environment.API_BASEURL + ENDPOINT.CREATE_NOTE;
+    return this.http.post(url, note)
+      .pipe(
+        map((response) => console.log(response))
+      )
+      .subscribe()
+
+  }
+
+  fetchNotes(notebookID: any) {
+    const url = environment.API_BASEURL + ENDPOINT.NOTES;
+    const noteParams = new HttpParams().append('notebookID', notebookID);
+    return this.http.get(url, { params: noteParams })
+      .pipe(
+        map((response: any) => response.data),
+        catchError(error => {
+          if(error.error.status == 'fail' && error.error.data.length === 0) {
+            // do nothing
+            return throwError(error);
+          }
+          else {
+            this.jtr.error();
+            return throwError(error);
+          }
+        })
+      )
+  }
+
+}
